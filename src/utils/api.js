c7004bfd15b80ee395c0c843f50fd1945d9df04e
Shrawan <shrwnkumar08@gmail.com>
2026-07-06 18:37:46 +0530

    adding user loging reducer

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,22 @@
+import axios from "axios";
+import * as secureStore from "expo-secure-store";
+
 export const Api = "http://192.168.29.134:3000/api";
+
+const axiosInstance = axios.create({baseURL : Api});
+
+
+//attach the token to every out going request automatically
+
+
+axiosInstance.interceptors.request.use(async (config)=>{
+    const token = await secureStore.getItemAsync("accessToken"); 
+
+    if(token)
+        config.headers.Authorization = `Bearer ${token}`;
+    
+    
+    return config;
+})
+
+export default axiosInstance;
